f39db50f99bfb2afcbe69e679dcf2a8e9bcbf84d
pitzee <pitzee421@gmail.com>
2023-10-08 18:20:21 +0000

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,13 +5,16 @@
 def canUnlockAll(boxes):
     # This function checks if all boxes can be unlocked.
 
-    number_of_keys = [0]
-    number_of_boxes = len(boxes)
+    position = 0
+    unlocked = {}
 
-    for keys in number_of_keys:
-        for box in boxes[keys]:
-            if box < number_of_boxes and box not in number_of_keys:
-                number_of_keys.append(box)
-    if len(number_of_keys) == number_of_boxes:
-        return True
+    for box in boxes:
+        if len(box) == 0 or position == 0:
+            unlocked[position] = "always_unlocked"
+        for key in box:
+            if key < len(boxes) and key != position:
+                unlocked[key] = key
+        if len(unlocked) == len(boxes):
+            return True
+        position += 1
     return False
